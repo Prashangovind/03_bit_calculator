154be475b910060ac36d10637eae04989bc0ecea
Prashangovind <111832043+Prashangovind@users.noreply.github.com>
2022-11-15 14:51:59 +1300

    Create 06_integer_bits.py

---
+++ 06_integer_bits.py
@@ -0,0 +1,48 @@
+def num_check(question, low):
+    valid = False
+    while not valid:
+
+        error = "Please enter an integer that is more than (or equal to) {}" .format(low)
+     
+        try:
+
+            # ask user to enter a number
+            response = int(input(question))
+
+            # checks that number is more than zero
+            if response >= low:
+                return response
+
+            # outputs error if inputs is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:    
+            print(error)
+   
+  
+  
+# converts decimal to binary and states how
+# many bits are needed to represent the original integer 
+def int_bits():
+
+    # get integer ( must be >= 0)
+    var_integer = num_check("Please enetr an integer: ", 0)
+
+    var_binary = "{0:b}".format(var_integer)
+
+    #calculate # of bits (length of string above)
+    num_bits = len(var_binary)
+
+     # output answer with working 
+    print()
+    print("{} in binary is {}".format(var_integer, var_binary))
+    print("# of bits is {}".format(num_bits))
+
+    return ""
+
+
+# main routine
+int_bits()
+    
